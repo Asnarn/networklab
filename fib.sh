dc42fdec5e67053e721c9a7edae95a0edf05f190
Asnarn <asnarn205@gmail.com>
2025-04-03 14:32:27 +0530

    Add files via upload

---
+++ fib.sh
@@ -0,0 +1,13 @@
+echo "enter the length of the fibbnocci series"
+read length
+num1=0
+num2=1
+echo -n "$num1 $num2"
+for (( i=2; i<length; i++ ));
+do
+next=$(( num1 + num2 ))
+echo -n "$next" 
+num1=$num2
+num2=$next
+done
+echo " "
